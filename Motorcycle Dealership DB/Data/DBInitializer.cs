4f5a3275879b2fccdfa16e3dc1b16b991b823104
Ahmad Muddassir Sooran <ac145088@avcol.school.nz>
2024-03-27 15:09:10 +1300

    added a initializer and am coding the correct models into it.

---
+++ Motorcycle Dealership DB/Data/DBInitializer.cs
@@ -0,0 +1,13 @@
+﻿namespace Motorcycle_Dealership_DB.Data
+{
+    public class DBInitializer
+    {
+        internal static void Initialize(Motorcycle_Dealership_DBContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //finding customers
+            if (context.Customer)
+        }
+    }
+}
--- Motorcycle Dealership DB/Models/Supplier.cs
+++ Motorcycle Dealership DB/Models/Supplier.cs
@@ -11,6 +11,6 @@
         public string Street { get; set; }
         public string City { get; set; }
         public int  Zip { get; set; }
-
+        
     }
 }
